5b80ca933b586342e49508ed9e2cd4467970bed3
Yuri Garcia <yurian.idani.garcia@clases.edu.sv>
2026-08-06 00:57:47 -0600

    prueba2

--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,12 @@ require('dotenv').config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
+  ssl: process.env.NODE_ENV === 'production' 
+    ? { rejectUnauthorized: false } 
+    : false,
+  // Parámetros para evitar cierres inesperados de sockets en Render
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 5000,
 });
 
 module.exports = pool;
